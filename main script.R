9f3070ace1c765ff3549e4811be3449ce28cbac7
Ben Harley <harleyb101010@gmail.com>
2024-07-24 22:50:16 +0100

    Libraries, Data Cleaning, and Waiting Time pipping added

---
+++ main script.R
@@ -0,0 +1,47 @@
+# Packages Loaded
+
+library(readxl)
+library(dplyr)
+library(fs)
+library(purrr)
+library(data.table)
+library(tidyr)
+library(janitor)
+library(stringr)
+library(readr)
+
+Data_Location <-"C:/Users/harle/OneDrive/Desktop/NHS Open Data R App/Data/Diagnostics_HB/diagnostics.csv" # File Path
+
+Diagnostics_Data <- read.csv(Data_Location) # Read in Data
+
+Diagnostics_Data_Cleanned <- Diagnostics_Data |> # Removing unrequired data on my end
+  select(-NumberOnListQF) #Removing NumberOnListQF from file
+
+
+###### Waiting Time Preparation pipping data through for waiting time critera
+
+Diagnostics_Waiting_Time_0_7 <- Diagnostics_Data_Cleanned |> 
+  filter(WaitingTime == "0-7 days") 
+
+Diagnostics_Waiting_Time_8_14 <- Diagnostics_Data_Cleanned|>
+  filter(WaitingTime == "8-14 days") 
+
+Diagnostics_Waiting_Time_15_21 <- Diagnostics_Data_Cleanned|>
+  filter(WaitingTime == "15-21 days") 
+
+Diagnostics_Waiting_Time_22_28 <- Diagnostics_Data_Cleanned|>
+  filter(WaitingTime == "22-28 days") 
+
+Diagnostics_Waiting_Time_29_35 <- Diagnostics_Data_Cleanned|>
+
+Diagnostics_Waiting_Time_36_42 <- Diagnostics_Data_Cleanned|>
+  filter(WaitingTime == "36-42 days") 
+
+Diagnostics_Waiting_Time_43_49 <- Diagnostics_Data_Cleanned|>
+  filter(WaitingTime == "43-49 days") 
+
+Diagnostics_Waiting_Time_50_56 <- Diagnostics_Data_Cleanned|>
+  filter(WaitingTime == "50-56 days") 
+
+###### Creating Summary Files for Future Dashboard Plotly Graphs and Maps
+
